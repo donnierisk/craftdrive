757cc1ff673b8312fce97387e278415cb6c69711
Donovan Risk <donnierisk@gmail.com>
2020-07-08 12:52:03 +0200

    remove comment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,10 @@
 <template>
   <div id="home">
     <employees />
-    <!-- <dialogue-option text:="text" :options="options" /> -->
   </div>
 </template>
 
 <script lang="ts">
-// @ is an alias to /src
-
 import { Component, Vue } from "vue-property-decorator";
 import Employees from "@/views/Employees.vue";
 
